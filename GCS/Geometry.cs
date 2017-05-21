04674f63c9211380af03ab49312c4b70cc5cd992
phillyai <sonagi.philly@gmail.com>
2017-05-22 00:59:01 +0900

    리팩토링

--- GCS/Geometry.cs
+++ GCS/Geometry.cs
@@ -12,14 +12,14 @@ namespace GCS
     {
         public static Vector2[] GetIntersect(Shape shape1, Shape shape2)
         {
-            if(shape1 is Line)
+            if (shape1 is Line)
             {
                 if (shape2 is Line)
                     return getIntersect(shape1 as Line, shape2 as Line);
                 else if (shape2 is Circle)
                     return getIntersect(shape1 as Line, shape2 as Circle);
             }
-            else if(shape1 is Circle)
+            else if (shape1 is Circle)
             {
                 if (shape2 is Line)
                     return getIntersect(shape2 as Line, shape1 as Circle);
@@ -27,80 +27,65 @@ namespace GCS
                     return getIntersect(shape1 as Circle, shape2 as Circle);
             }
             throw new Exception("뀨;;");
-           
+
         }
-        //교점이 없는 경우 빈 Vector2 배열 리턴
+
         private static Vector2[] getIntersect(Line line1, Line line2)
         {
-           
-            if (line1.grad == line2.grad && line1.yint==line2.yint) { throw new NotFiniteNumberException("일치"); }
 
-            else if (line1.grad == line2.grad) { return new Vector2[] { }; }
+            if (line1.Grad == line2.Grad && line1.Yint == line2.Yint) { throw new NotFiniteNumberException("일치"); }
 
-            float intersectx = (line2.yint - line1.yint) / (line1.grad - line2.grad);
-            return new Vector2[] { new Vector2(intersectx, intersectx * line1.grad + line1.yint) };
+            else if (line1.Grad == line2.Grad) { return new Vector2[] { }; }
+
+            float intersectx = (line2.Yint - line1.Yint) / (line1.Grad - line2.Grad);
+            return new Vector2[] { new Vector2(intersectx, intersectx * line1.Grad + line1.Yint) };
         }
 
         private static Vector2[] getIntersect(Line line, Circle circle)
         {
-            
-          
-            
-            float [] result= solve2Eq((float)Math.Pow(line.grad,2)+1,
-                2*line.grad*(line.yint-circle.Center.Y)-2*circle.Center.X,
-                (float)(Math.Pow(circle.Center.X,2)+Math.Pow(line.yint - circle.Center.Y, 2)-Math.Pow(circle.Radius,2)));
+            float[] result = solve2Eq((float)Math.Pow(line.Grad, 2) + 1,
+                2 * line.Grad * (line.Yint - circle.Center.Y) - 2 * circle.Center.X,
+                (float)(Math.Pow(circle.Center.X, 2) + Math.Pow(line.Yint - circle.Center.Y, 2) - Math.Pow(circle.Radius, 2)));
 
             if (result.Length == 0) { return new Vector2[] { }; }
-            //교점 없음.
-            else if (result.Length == 1) { return new Vector2[] {new Vector2(result[0],line.grad*result[0]+line.yint) }; }
-            //교점 1 개
+            else if (result.Length == 1) { return new Vector2[] { new Vector2(result[0], line.Grad * result[0] + line.Yint) }; }
             else
             {
-                return new Vector2[] {new Vector2(result[0],line.grad*result[0]+line.yint),
-                new Vector2(result[1],line.grad*result[1]+line.yint)};
-            }// 교점 2 개
-            throw new NotImplementedException();
-            
+                return new Vector2[] {new Vector2(result[0],line.Grad*result[0]+line.Yint),
+                new Vector2(result[1],line.Grad*result[1]+line.Yint)};
+            }
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="circle1"></param>
-        /// <param name="circle2"></param>
-        /// <returns></returns>
+
         private static Vector2[] getIntersect(Circle circle1, Circle circle2)
         {
-            float distance = (float)Math.Sqrt(Math.Pow(circle2.Center.X - circle1.Center.X,2) + Math.Pow(circle2.Center.Y - circle1.Center.Y,2));
+            float distance = (float)Math.Sqrt(Math.Pow(circle2.Center.X - circle1.Center.X, 2) + Math.Pow(circle2.Center.Y - circle1.Center.Y, 2));
 
-            if (distance==0) { throw new NotFiniteNumberException("일치"); }
+            if (distance == 0) { throw new NotFiniteNumberException("일치"); }
             else if (distance > circle1.Radius + circle2.Radius)//두 원이 밖에 있으면서 만나지 않음.
             {
                 return new Vector2[] { };
-                
+
             }
-            else if((float)Math.Abs(circle1.Radius-circle2.Radius)>distance)// 두 원 중 하나가 서로를 포함.
+            else if ((float)Math.Abs(circle1.Radius - circle2.Radius) > distance)// 두 원 중 하나가 서로를 포함.
             {
                 return new Vector2[] { };
             }
             else
             {
                 Line Centerline = new Line(circle1.Center, circle2.Center);
-                float subgrad = -1 / (Centerline.grad) ;
+                float subgrad = -1 / (Centerline.Grad);
 
+                var cos = cosThm(circle1.Radius, distance, circle2.Radius);
                 Vector2 subpoint = divpoint(
                     circle1.Center,
                     circle2.Center,
-                    (cosThm(circle1.Radius,distance, circle2.Radius)[0]*circle1.Radius) / distance,
-                    (cosThm(circle1.Radius, distance, circle2.Radius)[1]*circle2.Radius) / distance
+                    (cos.Item1 * circle1.Radius) / distance,
+                    (cos.Item2 * circle2.Radius) / distance
                     );
 
                 Line intersectline = new Line(subgrad, subpoint);
                 return getIntersect(intersectline, circle1);
-
             }
-            
-
-            throw new NotImplementedException();
         }
 
         private static float[] solve2Eq(float a, float b, float c)
@@ -115,29 +100,22 @@ namespace GCS
                 {
                     return new float[] { (-b + (float)Math.Sqrt(discriminant)) / 2 * a,
                         (-b - (float)Math.Sqrt(discriminant)) / 2 * a };
-                    //float 배열 리턴
                 }
-                
             }
-            
-        }
-        /// <summary>
-        /// 존나 섹시함
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <param name="c"></param>
-        /// <returns>{ cos a&b , cos b&c } 를 리턴</returns>
-        private static float[] cosThm(float a, float b, float c) 
-        {
-            float[] cos2=new float[] {(float)(Math.Pow(c,2)-Math.Pow(a,2)-Math.Pow(b,2))/2*a*b,
-            (float)(Math.Pow(a,2)-Math.Pow(b,2)-Math.Pow(a,2))/2*b*c};
-            return cos2;
         }
 
-        private static Vector2 divpoint(Vector2 p1, Vector2 p2,float m, float n)//p1과 p2를 m:n으로 내분하는 점의 좌표 리턴
+        private static (float, float) cosThm(float a, float b, float c)
         {
-            return new Vector2(m*p2.X+n*p1.X/(m+n), (m*p2.Y+n*p1.Y)/(m+n));
+            return ((float)(Math.Pow(c, 2) - Math.Pow(a, 2) - Math.Pow(b, 2)) / 2 * a * b,
+            (float)(Math.Pow(a, 2) - Math.Pow(b, 2) - Math.Pow(a, 2)) / 2 * b * c);
+        }
+
+        /// <summary>
+        /// p1과 p2를 m:n으로 내분하는 점의 좌표 리턴
+        /// </summary>
+        private static Vector2 divpoint(Vector2 p1, Vector2 p2, float m, float n)
+        {
+            return new Vector2((m * p2.X + n * p1.X) / (m + n), (m * p2.Y + n * p1.Y) / (m + n));
         }
     }
 }
