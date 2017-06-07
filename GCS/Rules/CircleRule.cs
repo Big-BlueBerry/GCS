3113e2b5ae35516a40cd06ebd965525d0752f92a
phillyai <sonagi.philly@gmail.com>
2017-06-07 16:32:42 +0900

    CircleRule, SegmentRule 추가
    존잘 갓우림 A.K.A 라그랑주님께서 실질적인 계산을 다 해줘야 겠음

---
+++ GCS/Rules/CircleRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace GCS.Rules
+{
+    public class CircleRule : IParentRule
+    {
+        public Circle Parent;
+        public Dot Dot { get; }
+        public event Action<Vector2> MoveTo;
+        protected float _angle;
+
+        public CircleRule(Dot dot, Circle parent)
+        {
+            parent.Moved += Parent_Moved;
+            Dot = dot;
+            Parent = parent;
+            _angle = -1; throw new WorkWoorimException("웅림앙 leftratio 구해죠..");
+        }
+
+        private void Parent_Moved()
+        {
+            throw new WorkWoorimException("점이 원의 angle에 맞춰서...해야댐");
+        }
+
+        public Vector2 FixedCoord(Vector2 original)
+        {
+            return Geometry.GetNearest(Parent, original);
+        }
+    }
+}
---
+++ GCS/Rules/SegmentRule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace GCS.Rules
+{
+    public class SegmentRule : IParentRule
+    {
+        public Segment Parent;
+        public Dot Dot { get; }
+        public event Action<Vector2> MoveTo;
+        protected float _leftRatio;
+
+        public SegmentRule(Dot dot, Segment parent)
+        {
+            parent.Moved += Parent_Moved;
+            Dot = dot;
+            Parent = parent;
+            _leftRatio = -1; throw new WorkWoorimException("웅림앙 leftratio 구해죠..");
+        }
+
+        private void Parent_Moved()
+        {
+            var p1 = Parent.Point1.Coord;
+            var p2 = Parent.Point2.Coord;
+            Vector2 moved = Vector2.Zero;
+            moved = new Vector2((p1.X * _leftRatio + p2.X * (1 - _leftRatio)) / (p1.X + p2.X),
+                                        (p1.Y * _leftRatio + p2.Y * (1 - _leftRatio)) / (p1.Y + p2.Y));
+
+            MoveTo?.Invoke(moved);
+        }
+
+        public Vector2 FixedCoord(Vector2 original)
+        {
+            return Geometry.GetNearest(Parent, original);
+        }
+    }
+}
