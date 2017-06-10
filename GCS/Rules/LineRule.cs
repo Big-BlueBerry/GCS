d9ba442fc270670b18aa644a0ed6a218c2e16eef
phillyai <sonagi.philly@gmail.com>
2017-06-10 20:03:24 +0900

    Line/Segment Rule 보완
    점이 움직여도 leftRatio 값이 변하지 않던 버그 수정
    새로운 점의 좌표 구할때 y좌표는 그대로 p1과 p2가 바뀌지 않은 버그 수정

--- GCS/Rules/LineRule.cs
+++ GCS/Rules/LineRule.cs
@@ -14,8 +14,14 @@ namespace GCS.Rules
         {
             parent.Moved += Parent_Moved;
             Dot = dot;
+            dot.Moved += Dot_Moved;
             Dot.Rule = this;
             Parent = parent;
+            _leftRatio = (Dot.Coord.X - Parent.Point1.Coord.X) / (Parent.Point2.Coord.X - Parent.Point1.Coord.X);
+        }
+
+        private void Dot_Moved()
+        {
             _leftRatio = (Dot.Coord.X - Parent.Point1.Coord.X) / (Parent.Point2.Coord.X - Parent.Point1.Coord.X);
         }
 
@@ -25,7 +31,7 @@ namespace GCS.Rules
             var p2 = Parent.Point2.Coord;
             Vector2 moved = Vector2.Zero;
             moved = new Vector2((p2.X * _leftRatio + p1.X * (1 - _leftRatio)),
-                                        (p1.Y * _leftRatio + p2.Y * (1 - _leftRatio)));
+                                        (p2.Y * _leftRatio + p1.Y * (1 - _leftRatio)));
             /* 
              * 우림 여기서 점이 라인 밖에 있으면 외분이잖아
              * 외분이면 _leftRatio가 0 미만 혹은 1 초과가 되는데 그래도 잘 작동할까?
--- GCS/Rules/SegmentRule.cs
+++ GCS/Rules/SegmentRule.cs
@@ -10,27 +10,38 @@ namespace GCS.Rules
         public event Action<Vector2> MoveTo;
         protected float _leftRatio;
 
+        bool parent_Moved = false;
+
         public SegmentRule(Dot dot, Segment parent)
         {
             parent.Moved += Parent_Moved;
             Dot = dot;
+            dot.Moved += Dot_Moved;
             Dot.Rule = this;
             Parent = parent;
             _leftRatio = (Dot.Coord.X - Parent.Point1.Coord.X) / (Parent.Point2.Coord.X - Parent.Point1.Coord.X);
         }
 
+        private void Dot_Moved()
+        {
+            if (!parent_Moved)
+                _leftRatio = (Dot.Coord.X - Parent.Point1.Coord.X) / (Parent.Point2.Coord.X - Parent.Point1.Coord.X);
+        }
+
         private void Parent_Moved()
-        {   
+        {
+            parent_Moved = true;
             var p1 = Parent.Point1.Coord;
             var p2 = Parent.Point2.Coord;
             Vector2 moved = Vector2.Zero;
             moved =  new Vector2((p2.X * _leftRatio + p1.X * (1 - _leftRatio)),
-                                        (p1.Y * _leftRatio + p2.Y * (1 - _leftRatio)));
+                                        (p2.Y * _leftRatio + p1.Y * (1 - _leftRatio)));
 
             Grid.Framework.Debug.WriteLine($"leftratio : {_leftRatio}");
             Grid.Framework.Debug.WriteLine(moved.ToString());
 
             MoveTo?.Invoke(moved);
+            parent_Moved = false;
         }
 
         public Vector2 FixedCoord(Vector2 original)
