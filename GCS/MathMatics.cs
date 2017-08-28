777f616e2dab5a3dffb8fc5554f4e5b2270f8b75
Lagrange <iftime1130@naver.com>
2017-08-28 16:11:43 +0900

    버그와 개선사항 수정

--- GCS/MathMatics.cs
+++ GCS/MathMatics.cs
@@ -24,28 +24,34 @@ namespace GCS
         {
             List<float> answer = new List<float>();
             List<float> coef = polynorm.ToList();
-            if (coef.Count == 0) return new float[] { }; 
+            if (coef.Count == 0) return new float[] { };
             else
             {
                 while (coef.First() == 0) coef.Remove(0);
             }
-            if (coef.Count == 1) return new float[] { };
+            if (coef.Count == 0) return new float[] { };
+            else if (coef.Count == 1) return new float[] { };
             else if (coef.Count == 2) return new float[] { -coef[0] / coef[1] };
             else if (coef.Count == 3) return solve2Eq(coef[0], coef[1], coef[2]);
             else
             {
                 var diff = new float[coef.Count - 1];
-                for (int i = 0; i < coef.Count - 1 ; i++)
+                for (int i = 0; i < coef.Count - 1; i++)
                 {
                     diff[i] = (coef.Count - 1 - i) * coef[i]; // 극값을 구한 후
                 }
                 List<float> extreme_vals = solveNEq(diff, accuracy).ToList();
                 extreme_vals.Sort();
+
                 extreme_vals.Insert(0, -Upperbound);
                 extreme_vals.Add(Upperbound);
+
                 for (int j = 0; j < extreme_vals.Count - 1; j++)//극값을 기준으로 구간을 나눠서 이진 탐색 실시
                 {
-                    if (applyPolynormialFunc(coef, extreme_vals[j]) * applyPolynormialFunc(coef, extreme_vals[j + 1]) < 0)
+                    if (applyPolynormialFunc(coef, extreme_vals[j]) < Math.Pow(10, -accuracy)) answer.Add(extreme_vals[j]);
+                    else if (applyPolynormialFunc(coef, extreme_vals[j + 1]) < Math.Pow(10, -accuracy)) answer.Add(extreme_vals[j + 1]);
+                    //중근을 먼저 찾고(중근은 극값안에 있음)
+                    else if (applyPolynormialFunc(coef, extreme_vals[j]) * applyPolynormialFunc(coef, extreme_vals[j + 1]) < 0)
                     {
                         float[] interval = new float[] { extreme_vals[j], extreme_vals[j + 1] };
                         while (Math.Abs(applyPolynormialFunc(coef, interval[0])) > Math.Pow(10, -accuracy))
@@ -61,10 +67,9 @@ namespace GCS
                     }
 
                 }
-                answer.ForEach((x) => { Math.Round(x, accuracy); });
+                answer.ForEach(x => Math.Round(x, accuracy));
                 return answer.ToArray();
             }
-            
         }
 
         private static float applyPolynormialFunc(List<float> polynorm, float x)
